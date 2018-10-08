ca329b8f1d62c0e5b4e32dad94b97e5f1ff372c4
Tr4vi5 <trav.dunn@outlook.com>
2018-10-08 13:56:01 -0500

    hitting recaptcha success

--- server/routes/applications.router.js
+++ server/routes/applications.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const axios = require('axios');
 
 /**
  * GET route template
@@ -13,7 +14,33 @@ router.get('/', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
+    if (req.body.captcha === undefined || req.body.captcha === '' || req.body.captcha === null){
+        return res.json({"success": false, "msg": "Please fill out captcha"});
+    }
 
+    // Secret Key
+    const secretKey = '6Ld9BHQUAAAAADgfahozNvUGW9a0Cohaz7RkAdHI';
+
+    //verify URL
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip=${req.connection.remoteAddress}`;
+
+    // make request to verifyUrl
+    axios({
+        method: 'POST',
+        url: verifyUrl,
+    }).then((response)=>{
+        body = response.data;
+        // if not successful
+        if (body.success !== undefined && !body.success) {
+            return res.json({ "success": false, "msg": "Failed captcha verification" });
+        }
+
+        //if successful
+        return res.json({ "success": true, "msg": "Captcha passed" });
+    }).catch((error) => {
+        console.log('ERROR', error);
+        res.sendStatus(500);
+    })
 });
 
 module.exports = router;
--- src/components/NewApplicationPage/NewApplicationPage.js
+++ src/components/NewApplicationPage/NewApplicationPage.js
@@ -1,9 +1,116 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-class NewApplicationPage extends Component {render() {
+class NewApplicationPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            application: {
+                applicant_first_name: '',
+                applicant_last_name: '',
+                applicant_address: '',
+                applicant_city: '',
+                applicant_state: '',
+                applicant_zipcode: '',
+                applicant_cell_phone: '',
+                applicant_email: '',
+                applicant_qualifications: '',
+                applicant_experience: '',
+                applicant_age_group: '',
+                resume: ''
+            }
+        }
+    }
+
+    postApplication = (e) => {
+        e.preventDefault();
+        console.log(document.querySelector('#g-recaptcha-response').value);
+        
+        const captcha = document.querySelector('#g-recaptcha-response').value;
+        axios({
+            method: 'POST',
+            url: '/applications',
+            // headers: {
+            //     'Accept': 'application/json, text/plain, */*',
+            //     'Content-type': 'application/json'
+            // },
+            data: {captcha: captcha}
+        }).then((response)=> {
+            console.log(response.data);
+        }).catch((error)=>{
+            console.log('Error in Application POST', error); 
+        })
+    }
+
+
+    render() {
         return (
             <div>
-                
+                <form onSubmit={this.postApplication}>
+                    <TextField
+                        required
+                        label="First Name"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Last Name"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Address"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="City"
+                        margin="normal"
+                    />
+
+
+                    <TextField
+                        required
+                        label="Zip Code"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Cell Phone"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Email Address"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Subject Area of Interest"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Applicable Qualifications"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Past Tutoring Experience"
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        label="Which Age Group do you Prefer to Teach?"
+                        margin="normal"
+                    />
+                    <div className="g-recaptcha" data-sitekey="6Ld9BHQUAAAAANG2ZTJ-tsZGsw9uaE1_1PTUKXlM"></div>
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </form>
             </div>
         )
     }
