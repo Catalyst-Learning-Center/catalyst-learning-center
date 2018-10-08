27a830b187b9ea138b29e3134eb989b345f9ab19
Abbey Janicek <abbey.janicek@gmail.com>
2018-10-08 11:23:57 -0500

    Updated database.sql with changes. Deleted random data sql files, waiting for confirmation for new data. Accidentally deleted and recreated README.

--- database.sql
+++ database.sql
@@ -11,9 +11,9 @@ CREATE TABLE "grade" (
 	"grade_level" varchar(1000) NOT NULL
 );
 
-CREATE TABLE "subject" (
+CREATE TABLE "subjects" (
 	"id" serial primary key NOT NULL,
-	"subject" varchar(1000) NOT NULL
+	"subjects" varchar(1000) NOT NULL
 );
 
 CREATE TABLE "schools" (
@@ -31,7 +31,7 @@ CREATE TABLE "location" (
 	"location_phone" varchar(1000) NOT NULL
 );
 
-CREATE TABLE "application_info" (
+CREATE TABLE "applications" (
 	"id" serial primary key NOT NULL,
 	"date" DATE NOT NULL,
 	"applicant_first_name" varchar(1000) NOT NULL,
@@ -42,7 +42,9 @@ CREATE TABLE "application_info" (
 	"applicant_zipcode" integer NOT NULL,
 	"applicant_cell_phone" varchar(1000) NOT NULL,
 	"applicant_email" varchar(1000) NOT NULL,
-	"applicant_skills" varchar(10000) NOT NULL,
+	"applicant_qualifications" varchar(10000) NOT NULL,
+	"applicant_experience" varchar(10000) NOT NULL,
+	"applicant_age_group" varchar(10000) NOT NULL,
 	"resume" varchar(10000) NOT NULL
 );
 
@@ -69,23 +71,46 @@ CREATE TABLE "sessions" (
 	"student_name" varchar(1000) NOT NULL,
 	"school_id" INT REFERENCES "schools" NOT NULL,
 	"grade_id" INT REFERENCES "grade" NOT NULL,
+	"subjects_id" INT REFERENCES "subjects" NOT NULL,
 	"start_time" TIME NOT NULL,
 	"end_time" TIME NOT NULL
 );
 
-CREATE TABLE "sessions_subject" (
+CREATE TABLE "sessions_subjects" (
 	"id" serial primary key NOT NULL,
 	"sessions_id" INT REFERENCES "sessions" NOT NULL,
-	"subject_id" INT REFERENCES "subject" NOT NULL
+	"subjects_id" INT REFERENCES "subjects" NOT NULL
+);
+
+CREATE TABLE "applications_subjects" (
+	"id" serial primary key NOT NULL,
+	"applications_id" INT REFERENCES "applications" NOT NULL,
+	"subjects_id" INT REFERENCES "subjects" NOT NULL
+);
+
+CREATE TABLE "user_info_subjects" (
+	"id" serial primary key NOT NULL,
+	"user_info_id" INT REFERENCES "user_info" NOT NULL,
+	"subjects_id" INT REFERENCES "subjects" NOT NULL
+);
+
+CREATE TABLE "applications_location" (
+	"id" serial primary key NOT NULL,
+	"applications_id" INT REFERENCES "applications" NOT NULL,
+	"location_id" INT REFERENCES "location" NOT NULL
+);
+
+CREATE TABLE "user_info_location" (
+	"id" serial primary key NOT NULL,
+	"user_info_id" INT REFERENCES "user_info" NOT NULL,
+	"location_id" INT REFERENCES "location" NOT NULL
 );
 
 INSERT INTO "grade" ("grade_level")
-VALUES ('K'), ('1'), ('2'), ('3'), ('4'), ('5'), ('6'), ('7'), ('8'), ('9'), ('10'), ('11'), ('12'), ('Other');
+VALUES ('K'), ('1'), ('2'), ('3'), ('4'), ('5'), ('6'), ('7'), ('8'), ('9'), ('10'), ('11'), ('12'), ('College'), ('Other');
 
-INSERT INTO "subject" ("subject")
-VALUES ('Grade 1 Math'), ('Grade 2 Math'), ('Grade 3 Math'), ('Grade 4 Math'), ('Grade 5 Math'), ('Grade 6 Math'),
-('Grade 7 Math'), ('Grade 8 Math'), ('Pre-Algebra'), ('Algebra'), ('Intermediate Algebra'), ('Geometry'), 
-('Algebra 2'), ('Trigonometry'), ('Pre-Calculus'), ('Calculus');
+INSERT INTO "subjects" ("subjects")
+VALUES ('Early Math'), ('Arithmetic'), ('Algebra'), ('Geometry'), ('Trigonometry'), ('Calculus'), ('Statistics & Probability'), ('College Topics');
 
 INSERT INTO "schools" ("school_name")
 VALUES ('Roosevelt'), ('Washburn'), ('Ubah Medical Academy'), ('Southwest'), ('Marcy Open School'), ('Lyndale'), ('Heritage'),
@@ -111,28 +136,28 @@ VALUES ('Roosevelt'), ('Washburn'), ('Ubah Medical Academy'), ('Southwest'), ('M
 INSERT INTO "location" ("location_name", "location_address", "location_city", "location_state", "location_zipcode", "location_phone")
 VALUES ('Franklin Library', '1314 E Franklin Ave', 'Minneapolis', 'MN', '55404', '612-543-6925'), ('Hosmer Library', '347 E 36th St', 'Minneapolis', 'MN', '55408', '612-543-6400');
 
-INSERT INTO "application_info" ("date", "applicant_first_name", "applicant_last_name", "applicant_address", "applicant_city", "applicant_state", "applicant_zipcode", "applicant_cell_phone", "applicant_email", "applicant_skills", "resume")
-VALUES ('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user11@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user12@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user13@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user14@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user15@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user16@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user17@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user18@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user19@email.com', 'math', 'resume url'),
-('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user20@email.com', 'math', 'resume url');
+INSERT INTO "applications" ("date", "applicant_first_name", "applicant_last_name", "applicant_address", "applicant_city", "applicant_state", "applicant_zipcode", "applicant_cell_phone", "applicant_email", "applicant_subjects", "applicant_qualifications", "applicant_experience", "applicant_age_group", "resume")
+VALUES ('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user11@email.com', 1, 'math', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user12@email.com', 2, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user13@email.com', 3, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user14@email.com', 4, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user15@email.com', 5, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user16@email.com', 6, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user17@email.com', 7, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user18@email.com', 8, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user19@email.com', 9, 'math', 'experience', 'kids', 'resume url'),
+('10/04/2018', 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user20@email.com', 10, 'math', 'experience', 'kids', 'resume url');
 
 -- Manually register ten users using the emails listed below before inserting this info into its table.
-INSERT INTO "user_info" ("user_id", "user_first_name", "user_last_name", "user_address", "user_city", "user_state", "user_zipcode", "user_cell_phone", "user_email", "user_skills", "resume")
-VALUES (1, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user1@email.com', 'math', 'resume url'),
-(2, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user2@email.com', 'math', 'resume url'),
-(3, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user3@email.com', 'math', 'resume url'),
-(4, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user4@email.com', 'math', 'resume url'),
-(5, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user5@email.com', 'math', 'resume url'),
-(6, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user6@email.com', 'math', 'resume url'),
-(7, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user7@email.com', 'math', 'resume url'),
-(8, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user8@email.com', 'math', 'resume url'),
-(9, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user9@email.com', 'math', 'resume url'),
-(10, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user10@email.com', 'math', 'resume url');
+INSERT INTO "user_info" ("user_id", "user_first_name", "user_last_name", "user_address", "user_city", "user_state", "user_zipcode", "user_cell_phone", "user_email", "user_subjects", "user_qualifications", "user_experience", "user_age_group", "resume")
+VALUES (1, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user1@email.com', 1, 'math', 'resume url'),
+(2, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user2@email.com', 'math', 2, 'experience', 'kids', 'resume url'),
+(3, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user3@email.com', 'math', 3, 'experience', 'kids', 'resume url'),
+(4, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user4@email.com', 'math', 4, 'experience', 'kids', 'resume url'),
+(5, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user5@email.com', 'math', 5, 'experience', 'kids', 'resume url'),
+(6, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user6@email.com', 'math', 6, 'experience', 'kids', 'resume url'),
+(7, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user7@email.com', 'math', 7, 'experience', 'kids', 'resume url'),
+(8, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user8@email.com', 'math', 8, 'experience', 'kids', 'resume url'),
+(9, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user9@email.com', 'math', 9, 'experience', 'kids', 'resume url'),
+(10, 'Jane', 'Smith', '123 Main Street', 'Minneapolis', 'MN', '55408', '612-555-5555', 'user10@email.com', 'math', 10, 'experience', 'kids', 'resume url');
 
