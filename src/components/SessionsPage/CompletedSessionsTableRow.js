4c2b44a521f6332283deede47414682e0bbe2a95
Julia Balliet <julia.m.balliet@gmail.com>
2018-10-11 12:03:21 -0500

    added conditional rendering for editing sessions

--- src/components/SessionsPage/CompletedSessionsTableRow.js
+++ src/components/SessionsPage/CompletedSessionsTableRow.js
@@ -3,41 +3,74 @@ import moment from 'moment';
 // Material UI imports
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import { Button, Input } from '@material-ui/core';
+import { Button, Input, TextField } from '@material-ui/core';
 
 class CompletedSessionsTableRow extends Component {
     constructor(props) {
         super(props);
         this.state = {
             editing: false,
+            session: {
+                session_date: this.props.n.session_date,
+                student_name: this.props.n.student_name,
+                school_name: this.props.n.school_name,
+                grade_level: this.props.n.grade_level,
+                subjects: this.props.n.subjects,
+                topics: this.props.n.topics,
+                time: this.props.n.time
+            }
         }
     }
+
+    toggleEdit = () => {
+        this.setState({
+            editing: !this.state.editing,
+        })
+    }
+
     render() {
         let content = null;
 
         if (this.state.editing) {
             content = (
-                <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={this.props.n.id}
-                >
+                <div>
                     <TableCell padding="checkbox">
-                        <Input type="date">{moment(this.props.n.session_date).format('MM/DD/YY')}</Input>
+                        {/* <Input type="date">{moment(this.props.n.session_date).format('MM/DD/YY')}</Input> */}
+                        <TextField
+                            type="date"
+                            defaultValue={moment(this.props.n.session_date).format('YYYY-MM-DD')}
+                        />
                     </TableCell>
-                    <TableCell component="th" scope="row" padding="none">
-                        {this.props.n.student_name}
-                    </TableCell>
-                    <TableCell numeric>{this.props.n.school_name}</TableCell>
-                    <TableCell numeric>{this.props.n.grade_level}</TableCell>
-                    <TableCell numeric>{this.props.n.subjects}</TableCell>
-                    <TableCell numeric>{this.props.n.topics}</TableCell>
-                    <TableCell numeric>{moment(this.props.n.time).format('h:mm:ss')}</TableCell>
                     <TableCell>
-                        <Button>Edit</Button>
+                        <TextField
+                            value={this.props.n.student_name}
+                        />
                     </TableCell>
-                </TableRow>
+                    <TableCell>
+                        <TextField
+                            value={this.props.n.school_name}
+                        />
+                    </TableCell>
+                    <TableCell>
+                        <TextField
+                            value={this.props.n.grade_level}
+                        />
+                    </TableCell>
+                    <TableCell>
+                        <TextField
+                            value={this.props.n.subjects}
+                        />
+                    </TableCell>
+                    <TableCell>
+                        <TextField
+                            value={this.props.n.topics}
+                        />
+                    </TableCell>
+                    <TableCell>{moment(this.props.n.time).format('h:mm:ss')}</TableCell>
+                    <TableCell>
+                        <Button onClick={this.toggleEdit}>Cancel</Button>
+                    </TableCell>
+                </div>
             )
         } else {
             content = (
@@ -59,14 +92,21 @@ class CompletedSessionsTableRow extends Component {
                     <TableCell numeric>{this.props.n.topics}</TableCell>
                     <TableCell numeric>{moment(this.props.n.time).format('h:mm:ss')}</TableCell>
                     <TableCell>
-                        <Button>Edit</Button>
+                        <Button onClick={this.toggleEdit}>Edit</Button>
                     </TableCell>
                 </TableRow>
             )
         }
 
         return (
-            {content}
+            <TableRow
+                hover
+                role="checkbox"
+                tabIndex={-1}
+                key={this.props.n.id}
+            >
+                {content}
+            </TableRow>
         )
     }
 }
