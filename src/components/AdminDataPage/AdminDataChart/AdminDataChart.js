e55424c72f664456fdd1d60b19ba9347d2088dc0
Abbey Janicek <abbey.janicek@gmail.com>
2018-10-11 14:20:07 -0500

    Created data chart file. Installed chartjs-2.

---
+++ src/components/AdminDataPage/AdminDataChart/AdminDataChart.js
@@ -0,0 +1,7 @@
+// import React, { Component } from 'react';
+// // import { connect } from 'react-redux';
+// import axios from 'axios';
+import Chart from 'chart.js';
+
+let myChart = new Chart(ctx, {});
+
