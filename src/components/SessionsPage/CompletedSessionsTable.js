13be0932b0122bceadf30fb19a1f4275f360d46c
Julia Balliet <julia.m.balliet@gmail.com>
2018-10-11 09:24:38 -0500

    set up completed sessions table row component

--- src/components/SessionsPage/CompletedSessionsTable.js
+++ src/components/SessionsPage/CompletedSessionsTable.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import classNames from 'classnames';
+import Axios from 'axios';
+// Material UI imports
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
-import { lighten } from '@material-ui/core/styles/colorManipulator';
-import Axios from 'axios';
-import moment from 'moment';
+// component imports
+import CompletedSessionsTableRow from './CompletedSessionsTableRow';
 
 let counter = 0;
 function createData(name, calories, fat, carbs, protein) {
@@ -297,6 +288,7 @@ class EnhancedTable extends React.Component {
                                 <TableCell>Subject</TableCell>
                                 <TableCell>Subtopic</TableCell>
                                 <TableCell>Total Time</TableCell>
+                                <TableCell></TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -305,27 +297,7 @@ class EnhancedTable extends React.Component {
                                 .map(n => {
                                     // const isSelected = this.isSelected(n.id);
                                     return (
-                                        <TableRow
-                                            hover
-                                            onClick={event => this.handleClick(event, n.id)}
-                                            role="checkbox"
-                                            // aria-checked={isSelected}
-                                            tabIndex={-1}
-                                            key={n.id}
-                                            // selected={isSelected}
-                                        >
-                                            <TableCell padding="checkbox">
-                                                {moment(n.session_date).format('MM/DD/YY')}
-                                            </TableCell>
-                                            <TableCell component="th" scope="row" padding="none">
-                                                {n.student_name}
-                                            </TableCell>
-                                            <TableCell numeric>{n.school_name}</TableCell>
-                                            <TableCell numeric>{n.grade_level}</TableCell>
-                                            <TableCell numeric>{n.subjects}</TableCell>
-                                            <TableCell numeric>{n.topics}</TableCell>
-                                            <TableCell numeric>{moment(n.time).format('h:mm:ss')}</TableCell>
-                                        </TableRow>
+                                        <CompletedSessionsTableRow n={n} />
                                     );
                                 })}
                             {emptyRows > 0 && (
