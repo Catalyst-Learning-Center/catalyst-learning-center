2e0f4679a3cfd4cf1429f280e1b1afd21aa6f023
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-10-18 14:26:01 -0500

    manage applications notification badge is displaying, counting, and updated the count on application removal.

--- src/components/AdminNav/AdminNav.js
+++ src/components/AdminNav/AdminNav.js
@@ -3,14 +3,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './AdminNav.css';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Badge from '@material-ui/core/Badge';
-import IconButton from '@material-ui/core/IconButton';
-import MailIcon from '@material-ui/icons/Mail';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 
 // Material UI imports
@@ -32,8 +26,34 @@ class AdminNav extends React.Component {
         this.state = {
             collapse: false,
             isWideEnough: false,
+            pendingApplications: 0,
         };
         this.onClick = this.onClick.bind(this);
+    }
+
+    // on page render, display the count of current pending applications
+    componentDidMount() {
+        this.getPendingApplications();
+    }
+
+    // when pending applications are updated, re-fetch, re-count, and re-render
+    componentDidUpdate() {
+        this.getPendingApplications();
+    }
+
+    // get the count of current pending applications
+    getPendingApplications = () => {
+        axios({
+            method: 'GET',
+            url: '/applications/pending'
+        }).then((response) => {
+            console.log(response.data)
+            this.setState({
+                pendingApplications: response.data[0].count
+            })
+        }).catch((error) => {
+            console.log('Error getting applications from the server: ', error);
+        });
     }
 
     onClick() {
@@ -51,39 +71,56 @@ class AdminNav extends React.Component {
     }
 
     render() {
+        // regular manage applications nav link without pending applications
+        let pendingItem = 
+        (<NavLink to="/manage-applications" activeStyle={style} >Manage Applications
+        &nbsp;
+        </NavLink>);
+
+        // if pending applications are more than 0, count and display them
+        if (this.state.pendingApplications > 0) {
+            pendingItem = (
+                <Badge badgeContent={this.state.pendingApplications} color="secondary">
+                    <NavLink to="/manage-applications" activeStyle={style}>
+                    Manage Applications
+                    &nbsp;
+                    </NavLink>
+                </Badge>
+            )
+        }
+
         return (
-                <Navbar className="admin-nav" dark expand="md" scrolling>
-                    <NavbarBrand tag="span">
-                        <strong className="admin-nav-brand">Catalyst Learning Center Admin</strong>
-                    </NavbarBrand>
-                    {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
-                    <Collapse isOpen={this.state.collapse} navbar>
-                        <NavbarNav left>
-                            <NavItem>
-                                <NavLink to="/admin-data" activeStyle={style}>View Data</NavLink>
-                            </NavItem>
-                            <NavItem>
+            <Navbar className="admin-nav" dark expand="md" scrolling>
+                <NavbarBrand tag="span">
+                    <strong className="admin-nav-brand">Catalyst Learning Center Admin {JSON.stringify(this.state.pendingApplications)}</strong>
+                </NavbarBrand>
+                {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
+                <Collapse isOpen={this.state.collapse} navbar>
+                    <NavbarNav left>
+                        <NavItem>
+                            <NavLink to="/admin-data" activeStyle={style}>View Data</NavLink>
+                        </NavItem>
+                        <NavItem>
                             <NavLink to="/manage-tutors" activeStyle={style}>Manage Tutors</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink to="/manage-applications" activeStyle={style} >Manage Applications &nbsp;
-                                <Badge badgeContent={4} color="secondary"></Badge>
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink to="/manage-locations" activeStyle={style}>Manage Locations</NavLink>
-                            </NavItem>
-                        </NavbarNav>
-                        <NavbarNav right>
-                            <NavItem>
-                                <Button variant="contained" onClick={this.startTutoring}>Start Tutoring</Button>
-                            </NavItem>
-                            <NavItem>
-                                <Button variant="contained" onClick={this.logout}>Logout</Button>
-                            </NavItem>
-                        </NavbarNav>
-                    </Collapse>
-                </Navbar>
+                        </NavItem>
+                        {/* return Manage Applications link and count */}
+                        <NavItem>
+                            {pendingItem}
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/manage-locations" activeStyle={style}>Manage Locations</NavLink>
+                        </NavItem>
+                    </NavbarNav>
+                    <NavbarNav right>
+                        <NavItem>
+                            <Button variant="contained" onClick={this.startTutoring}>Start Tutoring</Button>
+                        </NavItem>
+                        <NavItem>
+                            <Button variant="contained" onClick={this.logout}>Logout</Button>
+                        </NavItem>
+                    </NavbarNav>
+                </Collapse>
+            </Navbar>
         );
     }
 }
