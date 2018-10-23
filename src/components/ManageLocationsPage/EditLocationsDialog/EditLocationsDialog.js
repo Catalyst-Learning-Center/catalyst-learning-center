ff0c677b9c84b96cde0dc9a3e40f1f2db7d8c32c
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-10-22 22:02:42 -0500

    added 'required' to text fields via EditLocationDialog.js.

--- src/components/ManageLocationsPage/EditLocationsDialog/EditLocationsDialog.js
+++ src/components/ManageLocationsPage/EditLocationsDialog/EditLocationsDialog.js
@@ -118,6 +118,7 @@ const mapStateToProps = state => ({
             Please enter changes here:
           </DialogContentText>
           <TextField
+              required
               autoFocus
               margin="dense"
               name="location_name"
@@ -128,6 +129,7 @@ const mapStateToProps = state => ({
               onChange={this.handleChange}
             />
             <TextField
+              required
               autoFocus
               margin="dense"
               name="location_address"
@@ -138,6 +140,7 @@ const mapStateToProps = state => ({
               onChange={this.handleChange}
             />
             <TextField
+              required
               autoFocus
               margin="dense"
               name="location_city"
@@ -152,6 +155,7 @@ const mapStateToProps = state => ({
             defaultState={this.state.locationToEdit.location_state}
             />
             <TextField
+              required
               autoFocus
               margin="dense"
               name="location_zipcode"
@@ -162,6 +166,7 @@ const mapStateToProps = state => ({
               onChange={this.handleChange}
             />
             <TextField
+              required
               autoFocus
               margin="dense"
               name="location_phone"
